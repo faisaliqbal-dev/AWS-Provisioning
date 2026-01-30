6d7eb6a1040dfcf24ddc0f1907c57d4c5136eb3c
faisal <mdfaisaliqbal4257@gmail.com>
2026-01-31 02:35:43 +0530

    Changes in main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Application-Instance" {
     instance_type = "t3.micro"
     key_name = aws_key_pair.Application-Key-pair.key_name
     vpc_security_group_ids = [aws_security_group.Application_Sec_Grp.id]
-    subnet_id = data.aws_subnet.default.id
+    subnet_id = var.subnet_id
     count = 1
 
     user_data = <<-EOF
