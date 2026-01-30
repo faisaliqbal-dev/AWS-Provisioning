e2fb03162c4203031c58754e9f8e4ea38a52a0c9
faisal <mdfaisaliqbal4257@gmail.com>
2026-01-31 01:28:44 +0530

    Changes in Terraform

--- Terraform/key.tf
+++ Terraform/key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "Application-Key-pair" {
   key_name   = "Application-Key-pair"
-  public_key = file("${path.module}"/id_rsa.pub)
+  public_key = file("${path.module}/id_rsa.pub")
   
 }
