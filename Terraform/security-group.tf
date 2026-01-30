17d5cfb53216cace635afbd3b5ebb4f260cd1482
faisal <mdfaisaliqbal4257@gmail.com>
2026-01-31 01:52:02 +0530

    Changes in Terraform

--- Terraform/security-group.tf
+++ Terraform/security-group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "Application_Sec_Grp" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = "vpc-0ff774c27e04d046a"
 
   tags = {
     Name = "Application-Sec-Grp"
@@ -10,18 +10,19 @@ resource "aws_security_group" "Application_Sec_Grp" {
 
 resource "aws_vpc_security_group_ingress_rule" "Allow_SSH" {
   security_group_id = aws_security_group.Application_Sec_Grp.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+  cidr_ipv4        = var.cidr
 }
+
 
 resource "aws_vpc_security_group_ingress_rule" "Allow_nginx" {
   security_group_id = aws_security_group.Application_Sec_Grp.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+  cidr_ipv4      = var.cidr
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
