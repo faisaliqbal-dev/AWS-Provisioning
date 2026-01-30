7c79a1c729856156dc129f21ab7700390a0fc81e
faisal <mdfaisaliqbal4257@gmail.com>
2026-01-31 02:33:21 +0530

    Changes in main.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,6 +1,9 @@
 variable "cidr" {
   default = "0.0.0.0/0"
 }
-data "aws_subnet" "default" {
-  default_for_az = true
+
+variable "subnet_id" {
+  description = "Subnet ID for EC2 instance"
+  type        = string
+  default = "subnet-0c6f4ffb4fedd704b"
 }
